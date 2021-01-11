337d8fe83227a7f0105670d22ce8418f3292943e
Sakshi Choudhary <choudharysakshi023@gmail.com>
2021-01-12 01:25:31 +0530

    🔧 Configure TailwindCSS

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        darkOrange: "#E9573E",
+        lightOrange: "#FF644E",
+        baseBlue: "#5D9BEE",
+        baseBlack: "#222221",
+        baseGray: "#C4C4C4",
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
