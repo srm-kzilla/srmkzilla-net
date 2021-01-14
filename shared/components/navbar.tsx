432e0b1043abddb65ad4cbceef2f94e2214e9083
Sakshi Choudhary <choudharysakshi023@gmail.com>
2021-01-14 13:38:27 +0530

    ✨ add dropdown

--- shared/components/navbar.tsx
+++ shared/components/navbar.tsx
@@ -3,14 +3,18 @@ import { Search, Menu } from "react-feather";
 import Link from "next/link";
 
 const Navbar = () => {
-  const [open, setOpen] = useState(false);
+  const [open1, setOpen1] = useState(false);
   const [open2, setOpen2] = useState(false);
+  const [open3, setOpen3] = useState(false);
   const [search, setSearch] = useState(false);
   const btntoggle1 = () => {
-    setOpen(!open);
+    setOpen1(!open1);
   };
   const btntoggle2 = () => {
     setOpen2(!open2);
+  };
+  const btntoggle3 = () => {
+    setOpen3(!open3);
   };
   const openSearch = () => {
     setSearch(!search);
@@ -22,7 +26,6 @@ const Navbar = () => {
       setSearch(false);
     }
   };
-  
 
   useEffect(() => {
     window.addEventListener("scroll", searchshow);
@@ -38,7 +41,7 @@ const Navbar = () => {
             </div>
 
             <div className="flex md:hidden">
-              <div className="lg:hidden md:block">
+              <div className="lg:hidden flex flex-row">
                 <input
                   type="text"
                   name="search"
@@ -65,39 +68,57 @@ const Navbar = () => {
 
           <div
             className={`${
-              open ? "block" : "hidden"
-            } md:flex md:items-center md:justify-between flex-1`}
+              open1 ? "block" : "hidden"
+            } md:flex md:items-center md:justify-between w-full flex-1`}
           >
-            <div className="flex bg-opacity-20 flex-col -mx-4 md:flex-row md:items-center md:mx-8">
+            <div className="flex bg-opacity-20 flex-col md:flex-row md:items-center mx-5">
               <Link href="/">
-                <p className="md:mt-0 mx-2 mt-2 px-2 py-1 hover:border-lightOrange border-b-2 border-transparent">
+                <p className="md:mt-0 mx-2 mt-2 px-2 py-1 hover:border-lightOrange cursor-pointer border-b-2 border-transparent">
                   Home
                 </p>
               </Link>
               <Link href="/">
-                <p className="md:mt-0 mx-2 mt-2 px-2 py-1 hover:border-lightOrange border-b-2 border-transparent">
+                <p className="md:mt-0 mx-2 mt-2 px-2 py-1 hover:border-lightOrange cursor-pointer border-b-2 border-transparent">
                   Events
                 </p>
               </Link>
               <Link href="/">
-                <p className="md:mt-0 mx-2 mt-2 px-2 py-1 hover:border-lightOrange border-b-2 border-transparent">
+                <p className="md:mt-0 mx-2 mt-2 px-2 py-1 hover:border-lightOrange cursor-pointer border-b-2 border-transparent">
                   Our Services
                 </p>
               </Link>
               <Link href="/">
-                <p className="md:mt-0 mx-2 mt-2 px-2 py-1 hover:border-lightOrange border-b-2 border-transparent">
+                <p className="md:mt-0 mx-2 mt-2 px-2 py-1 hover:border-lightOrange cursor-pointer border-b-2 border-transparent">
                   Team
                 </p>
               </Link>
 
               <Link href="/">
-                <p className="md:mt-0 mx-2 mt-2 px-2 py-1 hover:border-lightOrange border-b-2 border-transparent">
+                <p
+                  className="md:mt-0 mx-2 mt-2 px-2 py-1 relative z-10 hover:border-lightOrange cursor-pointer border-b-2 border-transparent"
+                  onClick={btntoggle3}
+                >
                   What's New?
                 </p>
               </Link>
+              <div
+                className={`${
+                  open3 ? "block" : "hidden"
+                } absolute left-80 ml-10 mt-10 top-3 py-2 w-48 z-20 rounded-md shadow-md bg-baseBlack`}
+              >
+                <p className="block pointer-cursor px-4 py-2 text-sm text-white hover:text-lightOrange">
+                  projects
+                </p>
+                <p className="block pointer-cursor px-4 py-2 text-sm text-white hover:text-lightOrange">
+                  projects
+                </p>
+                <p className="block pointer-cursor px-4 py-2 text-sm text-white hover:text-lightOrange">
+                  projects
+                </p>
+              </div>
             </div>
 
-            <div className="flex md:flex-row items-center mt-4 md:mt-0">
+            <div className="md:flex-row mt-4 md:mt-0 origin-top-right hidden md:flex  absolute right-5">
               <input
                 type="text"
                 name="search"
@@ -122,16 +143,16 @@ const Navbar = () => {
               <div
                 className={`${
                   open2 ? "block" : "hidden"
-                } origin-top-right absolute right-10 top-10 mt-2 w-48 rounded-md shadow-md py-1 bg-baseBlack`}
+                } origin-top-right absolute right-1 top-5 mt-2 w-48 rounded-md shadow-md py-1 bg-baseBlack`}
               >
-                <p className="block px-4 py-2 text-sm text-white hover:text-lightOrange">
-                  projects
+                <p className="block pointer-cursor px-4 py-2 text-sm text-white hover:text-lightOrange">
+                  Es
                 </p>
-                <p className="block px-4 py-2 text-sm text-white hover:text-lightOrange">
-                  projects
+                <p className="block pointer-cursor px-4 py-2 text-sm text-white hover:text-lightOrange">
+                  Sp
                 </p>
-                <p className="block px-4 py-2 text-sm text-white hover:text-lightOrange">
-                  projects
+                <p className="block pointer-cursor px-4 py-2 text-sm text-white hover:text-lightOrange">
+                  Hn
                 </p>
               </div>
             </div>
