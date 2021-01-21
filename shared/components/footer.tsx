12b07b37026dbf836496c790d7703fc3f41e6bfa
Suritam-Nandy <13suritsuritam@gmail.com>
2021-01-21 15:35:06 +0530

    💄 make responsive

--- shared/components/footer.tsx
+++ shared/components/footer.tsx
@@ -1,84 +1,85 @@
-import React from 'react'
+import React from 'react';
+import styles from "./footer.module.css";
 import { Mail, Phone, Facebook, Twitter, Instagram, Linkedin } from 'react-feather';
 
 const footer = () => {
     return (
         <footer className="container mx-auto text-white rounded-xl mt-5 px-12 py-4 bg-footerBackground">
             
-           <div className="grid grid-cols-4 gap-4">
-                <div className="row-span-6">
+           <div className="grid lg:grid-cols-4 gap-4 sm:grid-cols-2">
+                <div className="row-span-6 mx-auto">
                     <img className="w-60" src="/images/KZILLA-logo-B-mono-1.png" alt="KZILLA-logo-B-mono-1"/>
             </div>
-                <div className="col-span-3 flex justify-end mr-8 mb-4">
+                <div className="lg:col-span-3 sm:col-span-1 flex justify-end mr-8 mb-4">
                     <Facebook className="inline-block mx-4" size={32}/>
                     <Twitter className="inline-block mx-4" size={32}/>
                     <Instagram className="inline-block mx-4" size={32}/>
                     <Linkedin className="inline-block mx-4" size={32}/>
                     
                  </div>
-                <div className="row-span-5">
-                <ul>
+                <div className="row-span-5 text-3xl">
+                <ul >
                         Explore
-                        <li  className="my-2">
+                        <li  className="my-2 text-2xl">
                         <a href="#"><span>Home</span></a>
                         </li>
-                        <li className="my-2">Events
+                        <li className="my-2 text-2xl">Events
                         <a href="#"><span>Events</span></a>
                          
                         </li>
-                        <li className="my-2">
+                        <li className="my-2 text-2xl">
                         <a href="#"><span>About us</span></a>
                          
                         </li>
-                        <li className="my-2">
+                        <li className="my-2 text-2xl">
                         <a href="#"><span>Our Services</span></a>
                          
                         </li>
-                        <li className="my-2">
-                        <a href="#"><span>Team</span></a>
+                        <li className="my-2 text-2xl">
+                        <a href="#"><span >Team</span></a>
                          
                         </li>
                         
                     </ul>
   </div>
-                <div className="row-span-5">
-                <ul>
+                <div className="row-span-5 text-3xl">
+                <ul >
                         What's new
-                        <li className="my-2">
+                        <li className="my-2 text-2xl">
                         <a href="#"><span>Projects</span></a>
                          
                         </li>
-                        <li className="my-2">
+                        <li className="my-2 text-2xl">
                         <a href="#"><span>YouTube</span></a>
                         </li>
-                        <li className="my-2">
+                        <li className="my-2 text-2xl">
                         <a href="#"><span>Blogs</span></a>
                         </li>
                         
                     </ul>
   </div>
-                <div className="row-span-5">
+                <div className="row-span-5 text-3xl">
                 <ul >
                         Get in touch
-                            <li className="my-2">
+                            <li className="my-2 text-2xl">
                             <Mail className="inline-block" size={18} />
                             
                             <a href="#"className="inline-block mx-1"><span>hello@srmkzilla.net</span></a>
                                  
                             </li>
-                            <li className="my-2">
+                            <li className="my-2 text-2xl">
                             <Phone className="inline-block" size={18} />
                             
                             <a href="#" className="inline-block mx-1"><span>Akshat Sinha</span></a>
                                 
                             </li>
-                            <li className="my-2">
+                            <li className="my-2 text-2xl">
                             <Phone className="inline-block" size={18} />
                             
                             <a href="#" className="inline-block mx-1"><span>Ishan Chabra</span></a>
                                 
                             </li>
-                            <li className="my-2 ">
+                            <li className="my-2 text-2xl">
                             <Phone className="inline-block" size={18} />
                             <a href="#" className="inline-block mx-1"><span>Sweta Nayak</span></a>
 
