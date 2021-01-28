70d5ea5449845c6dd1146016e61b71d983c6a2aa
Suritam-Nandy <13suritsuritam@gmail.com>
2021-01-28 17:06:38 +0530

    🚧 ✨ timeline

---
+++ pages/aboutus.tsx
@@ -0,0 +1,189 @@
+import React from 'react';
+import { Navbar } from "../shared/components";
+
+const aboutus = () => {
+    return (
+        <div
+            className="bg-black text-white w-full p-10 ">
+      <Navbar />
+          
+            <div className="conttainer text-white rounded-xl p-20  bg-black">
+               
+                <div className=" mx-auto font-subHeading font-style: normal mb-80 font-bold">
+                    <h1 className="subHeading not-italic font-bold text-6xl pt-40 pl-40">
+                    Welcome, Jack
+                    </h1>
+                </div>
+            </div>
+
+
+            <div className="grid grid-cols-3 md:gap-10 gap-14 md:ml-28 ml-56 mb-20">
+                <div className="w-3/4 h-96 overflow-hidden rounded-t-2xl rounded-b-2xl">
+                    <div className="bg-baseBlack   p-10">
+                        <div className="mb-10">
+                            <h1 className="text-4xl font-bold text-center">Values</h1>
+                            
+                        </div>
+                        <div className=" overflow-hidden">
+                            dsafgsdfgsdfdsfdsfjsdkjfhdk dsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk
+                        </div>
+                        
+                    </div>
+                </div>
+                <div className="w-3/4 h-96 overflow-hidden rounded-t-2xl rounded-b-2xl">
+                    <div className="bg-baseBlack   p-10">
+                        <div className="mb-10">
+                            <h1 className="text-4xl font-bold text-center">Mission</h1>
+                            
+                        </div>
+                        <div className=" overflow-hidden">
+                            dsafgsdfgsdfdsfdsfjsdkjfhdk dsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk
+                        </div>
+                        
+                    </div>
+                </div>
+
+                <div className="w-3/4 h-96 overflow-hidden rounded-t-2xl rounded-b-2xl">
+                    <div className="bg-baseBlack   p-10">
+                        <div className="mb-10">
+                            <h1 className="text-4xl font-bold text-center">Vision</h1>
+                            
+                        </div>
+                        <div className=" overflow-hidden">
+                            dsafgsdfgsdfdsfdsfjsdkjfhdk dsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk
+                        </div>
+                        
+                    </div>
+                </div>
+            </div>
+            
+
+            <div className="h-auto overflow-hidden bg-black text-white ">
+                <div className=" mx-auto font-subHeading font-style: normal mb-4 font-bold">
+                        <h2 className="subHeading not-italic font-bold text-7xl text-center text-white ">
+                        Our Virtue
+
+                        </h2>
+                </div>
+
+                <div className=" h-auto overflow-hidden flex items-center justify-center bg-black">
+                <div className="container bg-black mx-auto w-full h-full">
+      <div className="relative wrap overflow-hidden p-10 h-full">
+        <div
+          className=" absolute  left-1/2 border-white h-full border"
+        ></div>
+    {/* right timeline */}
+        <div
+          className="mb-8 flex justify-between items-center w-full right-timeline"
+        >
+          <div className="order-1 w-5/12"></div>
+          <div className="z-20 flex items-center order-1 bg-black border  w-6 h-6 rounded-full">
+            
+          </div>
+          <div
+            className="order-1 bg-gray-400 rounded-lg shadow-xl w-5/12 px-6 py-3"
+          >
+            <h3 className="mb-3 font-bold text-gray-800 text-xl">Lorem Ipsum</h3>
+            <p
+              className="text-sm leading-snug tracking-wide text-gray-900 text-opacity-100"
+            >
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book.
+            </p>
+          </div>
+        </div>
+
+        {/* left timeline */}
+        <div
+          className="mb-8 flex justify-between flex-row-reverse items-center w-full left-timeline"
+        >
+          <div className="order-1 w-5/12"></div>
+          <div className="z-20 flex items-center order-1 bg-black border  w-6 h-6 rounded-full">
+          </div>
+          <div className="order-1 bg-blue-400 rounded-lg shadow-xl w-5/12 px-6 py-4">
+            <h3 className="mb-3 font-bold text-white text-xl">Lorem Ipsum</h3>
+            <p
+              className="text-sm font-medium leading-snug tracking-wide text-white text-opacity-100"
+            >
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book.
+            </p>
+          </div>
+        </div>
+
+       {/* right timeline  */}
+        <div
+          className="mb-8 flex justify-between items-center w-full right-timeline"
+        >
+          <div className="order-1 w-5/12"></div>
+          <div className="z-20 flex items-center order-1 bg-black border  w-6 h-6 rounded-full">
+          </div>
+          <div
+            className="order-1 bg-gray-400 rounded-lg shadow-xl w-5/12 px-6 py-4"
+          >
+            <h3 className="mb-3 font-bold text-gray-800 text-xl">Lorem Ipsum</h3>
+            <p
+              className="text-sm leading-snug tracking-wide text-gray-900 text-opacity-100"
+            >
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book.
+            </p>
+          </div>
+        </div>
+
+     {/* left timeline  */}
+        <div
+          className="mb-8 flex justify-between flex-row-reverse items-center w-full left-timeline"
+        >
+          <div className="order-1 w-5/12"></div>
+          <div className="z-20 flex items-center order-1 bg-black border  w-6 h-6 rounded-full">
+          </div>
+          <div className="order-1 bg-blue-400 rounded-lg shadow-xl w-5/12 px-6 py-4">
+            <h3 className="mb-3 font-bold text-white text-xl">Lorem Ipsum</h3>
+            <p
+              className="text-sm font-medium leading-snug tracking-wide text-white text-opacity-100"
+            >
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+                </div>
+
+                
+                   
+
+            
+
+
+
+
+
+
+
+
+
+
+
+            </div>
+            
+
+
+
+
+      </div>
+        
+    )
+}
+
+export default aboutus
