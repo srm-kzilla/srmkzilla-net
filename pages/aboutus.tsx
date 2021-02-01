7db77c9f314569b55f115e31eb4c19bedaf9252c
Suritam-Nandy <13suritsuritam@gmail.com>
2021-02-02 00:00:26 +0530

    🚧 timeline

--- pages/aboutus.tsx
+++ pages/aboutus.tsx
@@ -60,114 +60,88 @@ const aboutus = () => {
 
             <div className="h-auto overflow-hidden bg-black text-white ">
                 <div className=" mx-auto font-subHeading font-style: normal mb-4 font-bold">
-                        <h2 className="subHeading not-italic font-bold text-7xl text-center text-white ">
-                        Our Virtue
+                                <h2 className="subHeading not-italic font-bold text-5xl text-center text-white ">
+                                Our Virtue
 
-                        </h2>
-                </div>
+                                </h2>
+                        </div>
 
-                <div className=" h-auto overflow-hidden flex items-center justify-center bg-black">
-                <div className="container bg-black mx-auto w-full h-full">
-      <div className="relative wrap overflow-hidden p-10 h-full">
-        <div
-          className=" absolute top-20 left-1/2 border-white h-full border"
-        ></div>
-    
-
-        {/* left timeline */}
-        <div className="mb-8 flex justify-between flex-row-reverse items-center w-full left-timeline">
-          <div className="order-1 w-1/2"></div>
-          <div className="z-20 flex  mb-10 items-center order-1 bg-black border  w-6 h-6 rounded-full">
-          </div>
-                  <div className="order-1 bg-black rounded-lg shadow-xl w-1/2  py-4">
-                   
-
-                              <div className="order-1 bg-blue-400 rounded-lg shadow-xl mx-2  px-6 py-4">
-                                  <h3 className="mb-3 w-full font-bold text-white text-xl">Lorem Ipsum</h3>
-                                  <p
-                                    className="text-sm font-medium leading-snug tracking-wide text-white text-opacity-100"
-                                  >
-                              
-                                </p>
-                              </div>
-                    
-
-
-                                  <div className="order-1 mt-12 bg-baseBlack rounded-lg shadow-xl  px-6 py-4">
-                                     <div className=" bg-white h-auto rounded-lg shadow-xl  px-6 py-4">
-                      
-                                      <h3 className="mb-3 w-full font-bold text-white text-xl">Lorem Ipsum</h3>
-                                     <p
-                                         className="text-sm font-medium leading-snug tracking-wide text-white text-opacity-100"
-                                       >
+                        <div className=" h-auto overflow-hidden flex items-center justify-center bg-black">
+                        <div className="container bg-black mx-auto w-full h-full">
+                            <div className="relative wrap overflow-hidden p-10 h-full">
+                              <div
+                                className=" absolute top-20 left-1/2 border-white h-full border"
+                              ></div>
                           
-                                     </p>
-                                 </div>                     
+
+                              {/* left timeline */}
+                              <div className="mb-8 flex justify-between flex-row-reverse items-center w-full left-timeline">
+                                <div className="z-20 flex  mb-96 items-center order-1 bg-black border  w-8 h-4 rounded-full">
+                                </div>
+                                        <div className="order-1 bg-black rounded-lg shadow-xl w-full  py-4">
                         
-                             </div>
+
+                                    <div className="order-1 bg-blue-400 rounded-lg shadow-xl mx-2  px-6 py-4">
+                                        <h3 className="mb-3 w-full font-bold text-white text-xl">Lorem Ipsum</h3>
+                                        <p
+                                          className="text-sm font-medium leading-snug tracking-wide text-white text-opacity-100"
+                                        >
+                                    
+                                      </p>
+                                    </div>
+                          
+
+
+                                        <div className="order-1 mt-12 mr-12 transform -rotate-2 h-max bg-baseBlack rounded-lg shadow-xl  px-6 py-4">
+                                          <div className="h-80 bg-white rounded-lg shadow-xl  px-6 py-4">
+                            
+                                            <h3 className="mb-3 w-full font-bold text-black text-xl">Lorem Ipsum</h3>
+                                          
+                                          </div> 
+                                            <p className="mt-10 text-sm font-medium leading-snug tracking-wide text-white text-opacity-100"              >
+                                              12th December 2020
+                                          </p>
+                          
+                            
+                              
+                                          </div>
+                                          </div>
+                                          <div className="mb-20 w-full ">
+                                                  <div className="p-14 w-full ">
+                                              <h3 className="mb-3 w-full text-white font-bold text-4xl">Mozohack</h3>
+                                              <p
+                                                        className="text-xl leading-relaxed tracking-widest "
+                                                      >
+                                                      A 3-day extravaganza aimed at spreading the message of the power of open-source web among the students is complete with workshops, technical quizzes, games and much more. Mozofest provides the perfect respite to students from their busy schedules.
+                                                      </p>
+                                              </div>
+                                        </div>
+                                    </div>
+                                    
                       
-              </div>
-                  
-                  
-                </div>
-                
-                <div className="mb-8 flex justify-between items-center w-full right-timeline">
-          <div className="order-1 w-5/12"></div>
-          <div className="z-20 flex items-center order-1 bg-black border  w-6 h-6 rounded-full">
-          </div>
-          <div
-            className="order-1 bg-gray-400 rounded-lg shadow-xl w-5/12 px-6 py-4"
-          >
-            <h3 className="mb-3 font-bold text-gray-800 text-xl">Lorem Ipsum</h3>
-            <p
-              className="text-sm leading-snug tracking-wide text-gray-900 text-opacity-100"
-            >
-             
-            </p>
-          </div>
-        </div>
+                                      <div className="mb-8 flex justify-between items-center w-full right-timeline">
+                                        <div className="order-1  w-1/2"></div>
+                                        <div className="z-20 mt-10  ml-4 flex items-center order-1 bg-black border  w-4 h-4 rounded-full">
+                                        </div>
+                                        <div
+                                          className="order-1  mb-10 ml-3 bg-gray-400 rounded-lg shadow-xl w-1/2  px-6 py-4"
+                                        >
+                                          <h3 className="mb-3 font-bold text-gray-800 text-xl">Lorem Ipsum</h3>
+                                          <p
+                                            className="text-sm leading-snug tracking-wide text-gray-900 text-opacity-100"
+                                          >
+                                          
+                                          </p>
+                                        </div>
+                              </div>
 
-       {/* right timeline  */}
-        <div
-          className="mb-8 flex justify-between items-center w-full right-timeline"
-        >
-          <div className="order-1 w-5/12"></div>
-          <div className="z-20 flex items-center order-1 bg-black border  w-6 h-6 rounded-full">
-          </div>
-          <div
-            className="order-1 bg-gray-400 rounded-lg shadow-xl w-5/12 px-6 py-4"
-          >
-            <h3 className="mb-3 font-bold text-gray-800 text-xl">Lorem Ipsum</h3>
-            <p
-              className="text-sm leading-snug tracking-wide text-gray-900 text-opacity-100"
-            >
-             
-            </p>
-          </div>
-        </div>
+                      
+                          </div>
+                         </div>
+                       </div>
 
-     {/* left timeline  */}
-        <div
-          className="mb-8 flex justify-between flex-row-reverse items-center w-full left-timeline"
-        >
-          <div className="order-1 w-5/12"></div>
-          <div className="z-20 flex items-center order-1 bg-black border  w-6 h-6 rounded-full">
-          </div>
-          <div className="order-1 bg-blue-400 rounded-lg shadow-xl w-5/12 px-6 py-4">
-            <h3 className="mb-3 font-bold text-white text-xl">Lorem Ipsum</h3>
-            <p
-              className="text-sm font-medium leading-snug tracking-wide text-white text-opacity-100"
-            >
-         
-            </p>
-          </div>
-        </div>
-      </div>
-    </div>
-                </div>
-
-                
-                   
+                                
+                                  
 
             
 
