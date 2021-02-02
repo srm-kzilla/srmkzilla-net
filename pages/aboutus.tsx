6ac2627a18705b2f662746daa07a839236de90d1
Suritam-Nandy <13suritsuritam@gmail.com>
2021-02-02 12:13:01 +0530

    💄 add timeline

--- pages/aboutus.tsx
+++ pages/aboutus.tsx
@@ -3,13 +3,11 @@ import { Navbar } from "../shared/components";
 
 const aboutus = () => {
     return (
-        <div
-            className="bg-black text-white w-full p-10 ">
-      <Navbar />
-          
-            <div className="conttainer text-white rounded-xl p-20  bg-black">
+        <div className="bg-black w-full text-white ">
+        <div  className="  w-full">
+            <div className="container  w-full text-white rounded-xl p-20  bg-black">
                
-                <div className=" mx-auto font-subHeading font-style: normal mb-80 font-bold">
+                <div className=" mx-auto  w-full font-subHeading font-style: normal mb-80 font-bold">
                     <h1 className="subHeading not-italic font-bold text-6xl pt-40 pl-40">
                     Welcome, Jack
                     </h1>
@@ -17,7 +15,7 @@ const aboutus = () => {
             </div>
 
 
-            <div className="grid grid-cols-3 md:gap-10 gap-14 md:ml-28 ml-56 mb-20">
+            <div className="grid grid-cols-3 md:gap-10 gap-14 md:ml-28 lg:ml-56 lg:mb-20">
                 <div className="w-3/4 h-96 overflow-hidden rounded-t-2xl rounded-b-2xl">
                     <div className="bg-baseBlack   p-10">
                         <div className="mb-10">
@@ -78,10 +76,10 @@ const aboutus = () => {
                               <div className="mb-8 flex justify-between flex-row-reverse items-center w-full left-timeline">
                                 <div className="z-20 flex  mb-96 items-center order-1 bg-black border  w-8 h-4 rounded-full">
                                 </div>
-                                        <div className="order-1 bg-black rounded-lg shadow-xl w-full  py-4">
+                                <div className="order-1 bg-black w-full  py-4">
                         
 
-                                    <div className="order-1 bg-blue-400 rounded-lg shadow-xl mx-2  px-6 py-4">
+                                    <div className="order-1 bg-baseBlue rounded-3xl shadow-xl mx-2  px-6 py-4">
                                         <h3 className="mb-3 w-full font-bold text-white text-xl">Lorem Ipsum</h3>
                                         <p
                                           className="text-sm font-medium leading-snug tracking-wide text-white text-opacity-100"
@@ -92,8 +90,8 @@ const aboutus = () => {
                           
 
 
-                                        <div className="order-1 mt-12 mr-12 transform -rotate-2 h-max bg-baseBlack rounded-lg shadow-xl  px-6 py-4">
-                                          <div className="h-80 bg-white rounded-lg shadow-xl  px-6 py-4">
+                                        <div className="order-1 mt-12 mr-12 transform -rotate-2 h-max bg-baseBlack rounded-3xl shadow-xl  px-6 py-4">
+                                          <div className="h-80 bg-white rounded-3xl shadow-xl  px-6 py-4">
                             
                                             <h3 className="mb-3 w-full font-bold text-black text-xl">Lorem Ipsum</h3>
                                           
@@ -105,7 +103,7 @@ const aboutus = () => {
                             
                               
                                           </div>
-                                          </div>
+                                 </div>
                                           <div className="mb-20 w-full ">
                                                   <div className="p-14 w-full ">
                                               <h3 className="mb-3 w-full text-white font-bold text-4xl">Mozohack</h3>
@@ -118,22 +116,94 @@ const aboutus = () => {
                                         </div>
                                     </div>
                                     
-                      
-                                      <div className="mb-8 flex justify-between items-center w-full right-timeline">
-                                        <div className="order-1  w-1/2"></div>
-                                        <div className="z-20 mt-10  ml-4 flex items-center order-1 bg-black border  w-4 h-4 rounded-full">
-                                        </div>
-                                        <div
-                                          className="order-1  mb-10 ml-3 bg-gray-400 rounded-lg shadow-xl w-1/2  px-6 py-4"
-                                        >
-                                          <h3 className="mb-3 font-bold text-gray-800 text-xl">Lorem Ipsum</h3>
-                                          <p
-                                            className="text-sm leading-snug tracking-wide text-gray-900 text-opacity-100"
-                                          >
-                                          
-                                          </p>
-                                        </div>
+                          {/* right timeline */}
+                          <div className="mb-8 flex justify-between items-center w-full right-timeline">
+                              <div className="z-20 flex  mb-96 items-center order-1 bg-black border  w-8 h-4 rounded-full">
                               </div>
+                              <div className="order-1 bg-black w-full  py-4">
+                  
+
+                                    <div className="order-1 bg-lightOrange rounded-3xl shadow-xl mx-2  px-6 py-4">
+                                  <h3 className="mb-3 w-full font-bold text-white text-xl">Lorem Ipsum</h3>
+                                 <p
+                                          className="text-sm font-medium leading-snug tracking-wide text-white text-opacity-100"
+                                        >
+                                    
+                                      </p>
+                                    </div>
+                          
+
+
+                                        <div className="order-1 mt-12 ml-12 transform rotate-2 h-max bg-baseBlack rounded-3xl shadow-xl  px-6 py-4">
+                                          <div className="h-80 bg-white rounded-3xl shadow-xl  px-6 py-4">
+                            
+                                            <h3 className="mb-3 w-full font-bold text-black text-xl">Lorem Ipsum</h3>
+                                          
+                                          </div> 
+                                            <p className="mt-10 text-sm font-medium leading-snug tracking-wide text-white text-opacity-100"              >
+                                              12th December 2020
+                                          </p>
+                          
+                            
+                              
+                                          </div>
+                                 </div>
+                                          <div className="mb-20 w-full ">
+                                                  <div className="p-14 w-full ">
+                                              <h3 className="mb-3 w-full text-white font-bold text-4xl">Mozohack</h3>
+                                              <p
+                                                        className="text-xl leading-relaxed tracking-widest "
+                                                      >
+                                                      A 3-day extravaganza aimed at spreading the message of the power of open-source web among the students is complete with workshops, technical quizzes, games and much more. Mozofest provides the perfect respite to students from their busy schedules.
+                                                      </p>
+                                              </div>
+                                        </div>
+                            </div>
+                          {/* left timeline */}
+                          <div className="mb-8 flex justify-between flex-row-reverse items-center w-full left-timeline">
+                                <div className="z-20 flex  mb-96 items-center order-1 bg-black border  w-8 h-4 rounded-full">
+                                </div>
+                                <div className="order-1 bg-black w-full  py-4">
+                        
+
+                                    <div className="order-1 bg-baseBlue rounded-3xl shadow-xl mx-2  px-6 py-4">
+                                        <h3 className="mb-3 w-full font-bold text-white text-xl">Lorem Ipsum</h3>
+                                        <p
+                                          className="text-sm font-medium leading-snug tracking-wide text-white text-opacity-100"
+                                        >
+                                    
+                                      </p>
+                                    </div>
+                          
+
+
+                                        <div className="order-1 mt-12 mr-12 transform -rotate-2 h-max bg-baseBlack rounded-3xl shadow-xl  px-6 py-4">
+                                          <div className="h-80 bg-white rounded-3xl shadow-xl  px-6 py-4">
+                            
+                                            <h3 className="mb-3 w-full font-bold text-black text-xl">Lorem Ipsum</h3>
+                                          
+                                          </div> 
+                                            <p className="mt-10 text-sm font-medium leading-snug tracking-wide text-white text-opacity-100"              >
+                                              12th December 2020
+                                          </p>
+                          
+                            
+                              
+                                          </div>
+                                 </div>
+                                          <div className="mb-20 w-full ">
+                                                  <div className="p-14 w-full ">
+                                              <h3 className="mb-3 w-full text-white font-bold text-4xl">Mozohack</h3>
+                                              <p
+                                                        className="text-xl leading-relaxed tracking-widest "
+                                                      >
+                                                      A 3-day extravaganza aimed at spreading the message of the power of open-source web among the students is complete with workshops, technical quizzes, games and much more. Mozofest provides the perfect respite to students from their busy schedules.
+                                                      </p>
+                                              </div>
+                                        </div>
+                            </div>
+
+
 
                       
                           </div>
@@ -159,7 +229,7 @@ const aboutus = () => {
             
 
 
-
+            </div>  
 
       </div>
         
