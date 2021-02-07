0b65a8f9b29578a76790dd5521b6ff8d4c007fc2
Sakshi Choudhary <choudharysakshi023@gmail.com>
2021-02-08 00:54:59 +0530

    ✨  add button (collaborate comp)

--- shared/components/services/collaborate.tsx
+++ shared/components/services/collaborate.tsx
@@ -4,7 +4,7 @@ const Collaborate = () => {
   return (
     <>
       <div className="flex flex-col md:flex-row text-white text-lg">
-        <div className="md:w-2/4 my-5 flex flex-col place-self-left">
+        <div className="md:w-2/4 my-5 flex-3 flex-col">
           Collaboration means challenging our potential and achieving a common
           goal. We can get together and bring out the best in us.
           <p className="mt-3">
@@ -12,8 +12,13 @@ const Collaborate = () => {
             <span className="font-bold text-orange600">Together</span> we can
             create something magnificent.
           </p>
+          <div className="my-10">
+            <button className="px-14 py-2 rounded-3xl bg-baseBlack border-2 border-white">
+              Get Started
+            </button>
+          </div>
         </div>
-        <div className="flex place-self-right">
+        <div className="flex-1">
           <img src="/images/thisisus.gif" className="w-max" />
         </div>
       </div>
