b5568af3acd44cc0f9d7d3db8cda8635a1f1d0d9
Sakshi Choudhary <choudharysakshi023@gmail.com>
2021-02-08 20:48:32 +0530

    🐛

--- shared/components/services/servicesprovided.tsx
+++ shared/components/services/servicesprovided.tsx
@@ -5,20 +5,19 @@ const ServicesProvided = () => {
     "SRMKZILLA has expertise in creating robust, scalable,responsive, and powerful web portals. This enables us to deliverweb portals that are a perfect amalgamation of features such ase-mails, chats, news, and information. If you can think it, wecan web it."
   );
   const [title, setTitle] = useState("Web Development");
-  const webdes = () => {
-    () =>
-      setDesc(
-        `Web design is the digital face of your organization. Quality web design helps you keep your leads on your page. The domain of web designing is one that is dynamic and is only limited by the designer's imagination. Hence, at SRMKZILLA, we strive to create aesthetically pleasing designs with a mint of creativity while still keeping up with the latest trends.`
-      );
-    setTitle("Web Design");
-  };
+
   const webdev = () => {
     setDesc(
       "SRMKZILLA has expertise in creating robust, scalable, responsive, and powerful web portals. This enables us to deliver web portals that are a perfect amalgamation of features such as e-mails, chats, news, and information. If you can think it, we can web it."
     );
     setTitle("Web Development");
   };
-
+  const webdes = () => {
+    setDesc(
+      "Web design is the digital face of your organization. Quality web design helps you keep your leads on your page. The domain of web designing is one that is dynamic and is only limited by the designers imagination. Hence, at SRMKZILLA, we strive to create aesthetically pleasing designs with a mint of creativity while still keeping up with the latest trends."
+    );
+    setTitle("Web Design");
+  };
   const gfx = () => {
     setDesc(
       "Design is thinking made visual. If the aesthetics are not spot-on, your audience will not give you a second glance. At SRMKZILLA, our Graphic Designing team ensures their outputs are a blend of knock-out graphics and smart marketing strategies. From a wide range of logos, brochures, banners, to design that empowers your web identity, we bring art to the cart. You call it illustration, we call it a sweeping victory."
