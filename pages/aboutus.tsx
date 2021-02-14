0380b209f8eddeca9f33c699749648694b746d81
Suritam-Nandy <13suritsuritam@gmail.com>
2021-02-14 12:08:12 +0530

    🎨

--- pages/aboutus.tsx
+++ pages/aboutus.tsx
@@ -1,19 +1,23 @@
 import React from 'react';
 import { Navbar } from "../shared/components";
+import Card_aboutus from "../shared/components/card_aboutus"
+import {LeftTimeline, RightTimeline } from "../shared/components/timeline_aboutus"
 import dynamic from "next/dynamic";
 
 const Card3d = dynamic(
   () => {
-    return import("../shared/3d_card");
+    return import("../shared/components/3d_card");
   },
   { ssr: false }
 );
 
+const cardData = [
+  { title: "Values" },
+  { title: "Mission" },
+  { title: "Mission" },
+];
 
 const aboutus =  () => {
-
-
-
     return (
       <div className="bg-black w-screen text-white ">
         <Navbar />
@@ -30,224 +34,32 @@ const aboutus =  () => {
 
 
             <div className="grid lg:grid-cols-3 sm:grid-cols-1  md:grid-cols-3 sm:gap-2 md:gap-3 gap-14 ml-16 md:ml-12 lg:ml-36 lg:mb-20">
-                <div className="w-3/4 h-96 overflow-hidden  rounded-t-2xl rounded-b-2xl">
-                    <div className="bg-baseBlack   p-10">
-                        <div className="mb-10">
-                            <h1 className="text-4xl font-bold text-center">Values</h1>
-                            
-                        </div>
-                        <div className=" overflow-hidden">
-                            dsafgsdfgsdfdsfdsfjsdkjfhdk dsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk
-                        </div>
-                        
-                    </div>
-                </div>
-                <div className="w-3/4 h-96 overflow-hidden rounded-t-2xl rounded-b-2xl">
-                    <div className="bg-baseBlack   p-10">
-                        <div className="mb-10">
-                            <h1 className="text-4xl font-bold text-center">Mission</h1>
-                            
-                        </div>
-                        <div className=" overflow-hidden">
-                            dsafgsdfgsdfdsfdsfjsdkjfhdk dsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk
-                        </div>
-                        
-                    </div>
-                </div>
-
-                <div className="w-3/4 h-96 overflow-hidden rounded-t-2xl rounded-b-2xl">
-                    <div className="bg-baseBlack   p-10">
-                        <div className="mb-10">
-                            <h1 className="text-4xl font-bold text-center">Vision</h1>
-                            
-                        </div>
-                        <div className=" overflow-hidden">
-                            dsafgsdfgsdfdsfdsfjsdkjfhdk dsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk
-                        </div>
-                        
-                    </div>
-                </div>
+            {cardData.map((card) => (
+              <Card_aboutus name={card.title}/>
+            ))}
             </div>
             
 
             <div className="h-auto overflow-hidden bg-black text-white lg:mx-20 ">
-                <div className=" mx-auto font-subHeading font-style: normal my-4 font-bold">
-                                <h2 className="subHeading not-italic font-bold text-4xl lg:text-5xl text-center text-white ">
-                                Our Virtue
+              <div className=" mx-auto font-subHeading font-style: normal my-4 font-bold">
+                <h2 className="subHeading not-italic font-bold text-4xl lg:text-5xl text-center text-white ">Our Virtue</h2>
+              </div>
 
-                                </h2>
-                        </div>
-
-                        <div className=" h-auto overflow-hidden flex items-center justify-center bg-black">
-                        <div className="container  w-full h-full">
-                            <div className="relative wrap overflow-hidden px-4 py-10 lg:p-10 h-full ">
-                              <div
-                                className=" absolute lg:top-20   left-1/2 border-white h-full border"
-                              ></div>
-                          
-
-                              {/* left timeline */}
-                              <div className="mb-8 flex justify-between flex-row-reverse items-center w-full left-timeline">
-                                <div className="z-20 flex mb-80 lg:mb-96 items-center order-1 bg-black border  w-8 h-4 rounded-full">
-                                </div>
-                                <div className="order-1 bg-black w-full  lg:py-4">
-                        
-
-                                    <div className="order-1 -mt-4 lg:mt-0 bg-baseBlue rounded-3xl shadow-xl lg:mx-2 px-6 py-3 lg:py-4">
-                                        <h3 className=" lg:mb-3 w-full font-bold text-white lg:text-xl">Lorem Ipsum</h3>
-                                        <p
-                                          className="text-sm font-medium leading-snug tracking-wide text-white text-opacity-100"
-                                        >
-                                    
-                                      </p>
-                                    </div>
-                          
-                                    
-
-
-
-                      <div className="effect3d__container moveable-3d order-1 mt-6 lg:mt-12 lg:mr-14 transform -rotate-2 w-full h-max bg-baseBlack rounded-3xl shadow-xl px-4 py-3  lg:px-6 lg:py-4" id="moveable-3d effect3d__container">
-                   
-                                          <div className="h-64 lg:h-80 bg-white rounded-3xl shadow-xl px-3  lg:px-6 lg:py-4">
-                            
-                                            <h3 className="lg:mb-3 w-full font-bold text-black lg:text-xl">Lorem Ipsum</h3>
-                                          
-                                          </div> 
-                                            <p className="lg:mt-10 text-sm font-medium leading-snug tracking-wide text-white text-opacity-100"              >
-                                              12th December 2020
-                                          </p>
-                          
-                            
-                                             
-                                          </div>
-                                 </div>
-                                          <div className="lg:mb-20 w-full ">
-                                                  <div className="lg:p-14 w-full ">
-                                              <h3 className="lg:mb-3 w-full text-white font-bold lg:text-4xl">Mozohack</h3>
-                                              <p
-                                                        className="lg:text-xl lg:leading-relaxed lg:tracking-widest "
-                                                      >
-                                                      A 3-day extravaganza aimed at spreading the message of the power of open-source web among the students is complete with workshops, technical quizzes, games and much more. Mozofest provides the perfect respite to students from their busy schedules.
-                                                      </p>
-                                              </div>
-                                              </div> 
-                                    </div>
-                                    
-                          {/* right timeline */}
-                          <div className="mb-8 flex justify-between items-center w-full right-timeline">
-                              <div className="z-20 flex mb-80 lg:mb-96 items-center order-1 bg-black border  w-8 h-4 rounded-full">
-                              </div>
-                              <div className="order-1 bg-black w-full  lg:py-4">
-                  
-
-                                    <div className="order-1 -mt-4 lg:mt-0 bg-orange600 rounded-3xl shadow-xl lg:mx-2 px-6 py-3 lg:py-4">
-                                  <h3 className="lg:mb-3 w-full font-bold text-white lg:text-xl">Lorem Ipsum</h3>
-                                 <p
-                                          className="text-sm font-medium leading-snug tracking-wide text-white text-opacity-100"
-                                        >
-                                    
-                                      </p>
-                                    </div>
-                          
-
-
-                                        <div className="effect3d__container moveable-3d order-1 mt-6 lg:mt-12 lg:mr-14 transform rotate-2 max-w-full h-max bg-baseBlack rounded-3xl shadow-xl px-4 py-3  lg:px-6 lg:py-4" id="moveable-3d effect3d__container">
-                                          <div className="h-64 lg:h-80 bg-white rounded-3xl shadow-xl px-3  lg:px-6 lg:py-4">
-                            
-                                            <h3 className="lg:mb-3 w-full font-bold text-black lg:text-xl">Lorem Ipsum</h3>
-                                          
-                                          </div> 
-                                            <p className="lg:mt-10 text-sm font-medium leading-snug tracking-wide text-white text-opacity-100">
-                                              12th December 2020
-                                          </p>
-                          
-                            
-                              
-                                          </div>
-                                 </div>
-                                          <div className="lg:mb-20 w-full ">
-                                                  <div className="lg:p-14 w-full ">
-                                              <h3 className="lg:mb-3 w-full text-white font-bold lg:text-4xl">Mozohack</h3>
-                                              <p
-                                                        className="lg:text-xl leading-relaxed lg:tracking-widest "
-                                                      >
-                                                      A 3-day extravaganza aimed at spreading the message of the power of open-source web among the students is complete with workshops, technical quizzes, games and much more. Mozofest provides the perfect respite to students from their busy schedules.
-                                                      </p>
-                                              </div>
-                                        </div>
-                            </div>
-                          {/* left timeline */}
-                          <div className="mb-8 flex justify-between flex-row-reverse items-center w-full left-timeline">
-                                <div className="z-20 flex mb-80 lg:mb-96 items-center order-1 bg-black border  w-8 h-4 rounded-full">
-                                </div>
-                                <div className="order-1 bg-black w-full  lg:py-4">
-                        
-
-                                    <div className="order-1 -mt-4 lg:mt-0 bg-baseBlue rounded-3xl shadow-xl lg:mx-2 px-6 py-3 lg:py-4">
-                                        <h3 className=" lg:mb-3 w-full font-bold text-white lg:text-xl">Lorem Ipsum</h3>
-                                        <p
-                                          className="text-sm font-medium leading-snug tracking-wide text-white text-opacity-100"
-                                        >
-                                    
-                                      </p>
-                                    </div>
-                          
-
-
-                                        <div className="moveable-3d effect3d__container order-1 mt-6 lg:mt-12 lg:mr-14 transform -rotate-2 max-w-full h-max bg-baseBlack rounded-3xl shadow-xl px-4 py-3  lg:px-6 lg:py-4" id="moveable-3d effect3d__container">
-                                          <div className="h-64 lg:h-80 bg-white rounded-3xl shadow-xl px-3  lg:px-6 lg:py-4">
-                            
-                                            <h3 className="lg:mb-3 w-full font-bold text-black lg:text-xl">Lorem Ipsum</h3>
-                                          
-                                          </div> 
-                                            <p className="lg:mt-10 text-sm font-medium leading-snug tracking-wide text-white text-opacity-100"              >
-                                              12th December 2020
-                                          </p>
-                          
-                            
-                              
-                                          </div>
-                                 </div>
-                                          <div className="lg:mb-20 w-full ">
-                                                  <div className="lg:p-14 w-full ">
-                                              <h3 className="lg:mb-3 w-full text-white font-bold lg:text-4xl">Mozohack</h3>
-                                              <p
-                                                        className="lg:text-xl lg:leading-relaxed lg:tracking-widest "
-                                                      >
-                                                      A 3-day extravaganza aimed at spreading the message of the power of open-source web among the students is complete with workshops, technical quizzes, games and much more. Mozofest provides the perfect respite to students from their busy schedules.
-                                                      </p>
-                                              </div>
-                                        </div>
-                                    </div>
-
-
+              <div className=" h-auto overflow-hidden flex items-center justify-center bg-black">
+                <div className="container  w-full h-full">
+                  <div className="relative wrap overflow-hidden px-4 py-10 lg:p-10 h-full ">
+                    <div className=" absolute lg:top-20   left-1/2 border-white h-full border"></div>
+                    {/* timeline */}
+                    <LeftTimeline />
+                    <RightTimeline/>
+                    <LeftTimeline/>
 
                       
-                          </div>
-                         </div>
-                       </div>
-
-                                
-                                  
-
-            
-
-
-
-
-
-
-
-
-
-
-
+                  </div>
+                </div>
+              </div>
             </div>
-            
-
-
-            </div>  
-
+          </div>  
       </div>
         
     )
---
+++ shared/components/card_aboutus.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Card_aboutus = (props: { name: String }) => {
+    return (
+        <div className="w-3/4 h-96 overflow-hidden rounded-t-2xl rounded-b-2xl">
+            <div className="bg-baseBlack   p-10">
+                <div className="mb-10">
+                            <h1 className="text-4xl font-bold text-center">{props.name}</h1>
+                            
+                </div>
+                <div className=" overflow-hidden">
+                            dsafgsdfgsdfdsfdsfjsdkjfhdk dsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk dsafgsdfgsdfdsfdsfjsdkjfhdk
+                </div>
+                        
+            </div>
+        </div>
+    )
+}
+
+export default Card_aboutus
