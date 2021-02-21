a00a69d996f470b113c67dbdbef6f8813f66ba88
Sakshi Choudhary <choudharysakshi023@gmail.com>
2021-02-21 10:13:14 +0530

    💬 team content

--- pages/team.tsx
+++ pages/team.tsx
@@ -47,8 +47,8 @@ const Team = () => {
           <h1 className="text-4xl font-semibold text-gray-100 text-center mb-10 mx-2">
             Meet the team
           </h1>
-          <h3 className="text-2xl text-gray-100 text-center">
-            syadjgewkfewvasiugs
+          <h3 className="text-xl text-gray-100 text-center">
+            An awesome tech community driven by passion and innovation
           </h3>
         </div>
         <div className=" text-white rounded-2xl pt-8 px-1 md:p-10 bg-black-200 grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5 lg:gap-2 md:mx-20 mx-2">
@@ -73,9 +73,17 @@ const Team = () => {
         </div>
 
         <div className="my-10 px-0 md:px-10">
-          <h1 className="text-4xl font-semibold text-gray-100 text-center my-20">
-            Hear it form the team
-          </h1>
+          <div className="flex flex-col place-items-center my-20">
+            <h1 className="text-4xl font-semibold text-gray-100 text-center">
+              Hear it form the team
+            </h1>
+            <h3 className="text-xl w-2/3 text-gray-100 text-center mt-5">
+              The journey of building an awesome tech community is fuelled by
+              the ardour of its team members. At SRMKZILLA, their ride has been
+              full of incredible experiences to inspire and aspire. Let us hear
+              what they have to say!
+            </h3>
+          </div>
           <NewCarousel />
         </div>
       </div>
