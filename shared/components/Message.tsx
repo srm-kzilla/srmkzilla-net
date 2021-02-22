7c369390a70a574dccf7324b36d95e9c0a92929a
harshgoel05 <harshgoel05@gmail.com>
2021-02-22 19:50:50 +0530

    🍻 Remove unused imports

--- shared/components/Message.tsx
+++ shared/components/Message.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { motion } from 'framer-motion'
-import { InView, useInView } from 'react-intersection-observer'
 
 const container = {
   hidden: { opacity: 1 },
@@ -28,14 +27,10 @@ const Message = (props: { color: String; message: String }) => {
         className="absolute right-0 transform left-full ml-5 sm:top-5 top-2/4"
         src="./images/messageLogo.png"
         alt="logo"
-        draggable = {false}
+        draggable={false}
       />
+      <motion.div className="absolute border-white w-full h-full top-0 left-0 rounded-xl border transform -translate-y-1 -translate-x-1 z-10"></motion.div>
       <motion.div
-        
-        className="absolute border-white w-full h-full top-0 left-0 rounded-xl border transform -translate-y-1 -translate-x-1 z-10"
-      ></motion.div>
-      <motion.div
-        
         className={`corner h-5 w-6 bg-${props.color} absolute right-0 transform translate-x-2/4 rounded-bl-2xl`}
       >
         <motion.div className="block bg-black-200 h-4 w-3 float-right rounded-bl-xl"></motion.div>
