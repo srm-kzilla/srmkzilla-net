6b57a7ef8b002262bb460464bde8d514f2c5bbb8
Shrey Sachdeva <shreysachdeva.2000@gmail.com>
2021-02-25 01:54:34 +0530

    ⚡️ use getStaticProps to fetch CMS content

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,16 +1,78 @@
-import dynamic from 'next/dynamic'
-import Loader from '../shared/components/loader'
-const AboutPage = dynamic(() => import('../shared/components/about'), {
-  ssr: false,
-  loading: () => <Loader />,
-})
+import React from 'react'
+import Head from 'next/head'
+const Fade = require('react-reveal/Fade')
 
-const index = () => {
+import Navbar from '@shared/components/navbar'
+import SwipeCard from '@shared/components/about/swipe-cards'
+import { LeftTimeline, RightTimeline } from '@shared/components/about/timeline'
+import Footer from '@shared/components/footer'
+import { getTimeline } from '@lib/sanity-api'
+import { CARDS } from '@lib/data/about'
+
+const AboutUs = ({ timelineData }) => {
   return (
-    <div>
-      <AboutPage />
+    <div className="bg-black text-white overflow-hidden">
+      <Head>
+        <title>SRMKZILLA | About</title>
+      </Head>
+      <Navbar />
+
+      <div className="md:mt-12">
+        <div className="md:h-screen h-auto items-center flex justify-center">
+          <div className="md:h-3/4 h-full sm:w-10/12 w-full md:mb-16 mb-8 rounded-none md:rounded-xl overflow-hidden">
+            <img
+              src="/images/mozofest.png"
+              draggable={false}
+              alt="image"
+              className="object-contain"
+            />
+          </div>
+        </div>
+
+        <div
+          className="flex flex-wrap
+         items-center justify-center"
+        >
+          {CARDS.map((card) => {
+            return <SwipeCard content={card} key={card.name} />
+          })}
+        </div>
+
+        <div className="h-auto overflow-hidden bg-black text-white lg:mx-auto mt-14 md:mt-20">
+          <Fade up>
+            <h2 className="mx-auto font-medium my-4 text-4xl lg:text-5xl text-center text-white ">
+              Our Virtues
+            </h2>
+          </Fade>
+
+          <div className=" h-auto overflow-hidden flex items-center justify-center bg-black">
+            <div className="container  w-full h-full">
+              <div className="relative wrap overflow-hidden pr-3 py-10 lg:p-10 h-full mx-4 md:mx-16 lg:mx-10">
+                <div className=" absolute lg:top-20 invisible md:visible lg:visible md:left-1/2 lg:left-1/2 border-white h-full border"></div>
+                {timelineData.length > 0 &&
+                  timelineData.map((timeline, index) =>
+                    index % 2 == 0 ? (
+                      <RightTimeline data={timeline} key={index} />
+                    ) : (
+                      <LeftTimeline data={timeline} key={index} />
+                    )
+                  )}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Footer />
     </div>
   )
 }
 
-export default index
+export async function getStaticProps() {
+  const timelineData = await getTimeline()
+
+  return {
+    props: { timelineData },
+  }
+}
+
+export default AboutUs
--- pages/team.tsx
+++ pages/team.tsx
@@ -1,12 +1,79 @@
-import dynamic from 'next/dynamic'
-import Loader from '../shared/components/loader'
-const TeamPage = dynamic(() => import('../shared/components/team'), {
-  ssr: false,
-  loading: () => <Loader />,
-})
+import Head from 'next/head'
 
-const index = () => {
-  return <TeamPage />
+import Navbar from '@shared/components/navbar'
+import MemberCard from '@shared/components/team/membercard'
+import NewCarousel from '@shared/components/team/carousel'
+import Footer from '@shared/components/footer'
+import { getTeamMembers } from '@lib/sanity-api'
+
+const Team = ({ teamMembers }) => {
+  return (
+    <>
+      <Head>
+        <title>SRMKZILLA | Team</title>
+      </Head>
+      <div className="bg-black overflow-hidden ">
+        <Navbar />
+        <p>fhjfvk</p>
+        <div className="my-20">
+          <h1 className="text-4xl font-semibold text-gray-100 text-center mb-10 mx-2">
+            Meet the team
+          </h1>
+          <h3 className="text-2xl text-gray-100 text-center px-5">
+            An awesome tech community driven by passion and innovation
+          </h3>
+        </div>
+        <div className=" text-white rounded-2xl pt-8 px-1 md:p-10 bg-black-200 grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5 lg:gap-2 md:mx-20 mx-2">
+          {teamMembers.length > 0 &&
+            teamMembers.map(
+              (
+                member: {
+                  picture: { asset: { url: string } }
+                  name: string
+                  designation: string
+                  description: { asset: { url: string } }
+                },
+                index
+              ) => {
+                return (
+                  <MemberCard
+                    key={index}
+                    src={member?.picture?.asset?.url}
+                    name={member?.name}
+                    designation={member?.designation}
+                    audiourl={member?.description?.asset?.url}
+                  />
+                )
+              }
+            )}
+        </div>
+
+        <div className="my-10 px-0 md:px-10">
+          <div className="flex flex-col place-items-center my-20">
+            <h1 className="text-4xl font-semibold text-gray-100 text-center">
+              Hear it form the team
+            </h1>
+            <h3 className="w-2/3 text-sm md:text-lg text-gray-100 text-center mt-5">
+              The journey of building an awesome tech community is fuelled by
+              the ardour of its team members. At SRMKZILLA, their ride has been
+              full of incredible experiences to inspire and aspire. Let us hear
+              what they have to say!
+            </h3>
+          </div>
+          <NewCarousel />
+        </div>
+      </div>
+      <Footer />
+    </>
+  )
 }
 
-export default index
+export async function getStaticProps() {
+  const teamMembers = await getTeamMembers()
+
+  return {
+    props: { teamMembers },
+  }
+}
+
+export default Team
