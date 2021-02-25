9e420e9a3492b335019b0eb82e62696b3e66e522
Sakshi Choudhary <choudharysakshi023@gmail.com>
2021-02-25 16:32:02 +0530

    🎨 data in lib

--- shared/components/team/carousel.tsx
+++ shared/components/team/carousel.tsx
@@ -1,28 +1,12 @@
 import React from 'react'
 import Carousel from 'react-elastic-carousel'
-
-const OldTeamMembers = [
-  {
-    name: 'Ayush Ashatkar',
-    src: '/images/ayushashatkar.jpg',
-    designation: 'Secretary / Club captain',
-    message: `Srmkzilla is place to grow, code & contribute on open source platform. It was a rollercoaster ride for me, where I learn about both technical and management skills and I can proudly say that it's a team dedication and smart work because of which it is the best technical club in south Indian region.This club play an influential role in my college journey and I'm proud to be a part of the club.`,
-  },
-  {
-    name: 'Meenakshi Sharma',
-    src: '/images/meenakshi.jpg',
-    designation: 'Secretary',
-    message: `Srmkzilla has been an integral part of my college experience. Without it, my journey definitely would not have been the same. It introduced me to so many new avenues, and more importantly, to so many amazing and interesting personalities that still continue to teach and influence me. It was, and always will be, an honor to be a part of the club!`,
-  },
-]
+import { alumniReviews } from '@lib/data/reviews'
 
 const NewCarousel = () => {
-  const myArrow = () => {}
-
   return (
     <div>
       <Carousel enableAutoPlay autoPlaySpeed={30000} isRTL={false}>
-        {OldTeamMembers.map((item) => {
+        {alumniReviews.map((item) => {
           return (
             <div>
               <div className="relative mx-auto bg-black-100 md:w-2/4 w-10/12 rounded-xl pb-14  pt-24">
