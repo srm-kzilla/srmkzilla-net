78995dfa13536c16ed2e0b637eb978d4ba7642d9
Sakshi Choudhary <choudharysakshi023@gmail.com>
2021-02-25 21:14:07 +0530

    🍱 add review

--- lib/data/reviews.ts
+++ lib/data/reviews.ts
@@ -11,4 +11,12 @@ export const alumniReviews = [
     designation: 'Secretary',
     message: `Srmkzilla has been an integral part of my college experience. Without it, my journey definitely would not have been the same. It introduced me to so many new avenues, and more importantly, to so many amazing and interesting personalities that still continue to teach and influence me. It was, and always will be, an honor to be a part of the club!`,
   },
+  {
+    name: 'Parv',
+    src: '/images/parv.jpg',
+    designation: 'Joint Secretary',
+    message: `SRMKZILLA is one of the most hardworking and successful club of SRM.
+    SRMKZILLA is a place of amazing developer where you can explore, learn and implement.
+     Leading this club was amazing feeling and organizing MOZOFEST was one of my favourite days during college time.`,
+  },
 ]
