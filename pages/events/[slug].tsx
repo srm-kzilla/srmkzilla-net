d919b80a4fbd5a58cc3566aa8872745791f6f331
CampVamp <ajayramsaravanan4@gmail.com>
2023-01-04 23:59:00 +0530

    :sparkles: Add Prizes Component in [slug].tsx

--- pages/events/[slug].tsx
+++ pages/events/[slug].tsx
@@ -65,67 +65,7 @@ export async function getServerSideProps(context: { params: { slug: any } }) {
     }
   }
   return {
-    // props: eventDetails[0],
-    props: {
-      _id: '634e69e9ba83a43e057ac28a',
-      description:
-        'Tune in for a spooktacular 5 day online event in association with Hacktoberfest from 21st October to 28th October.\nFrom a dazzling interactive workshop to thrilling coding challenges spread across different levels, you’re in for a one of a kind treat (or trick :p)\n\nThe top 3 spookers in GitHub repository contributions win insane rewards! 🍬 Don’t worry, the top 50 github contributors get horrifyingly awesome goodies too! 🕸',
-      description2:
-        'Register below to get detailed knowledge and learn about Open Source software',
-      eventCover:
-        'https://s3.ap-south-1.amazonaws.com/events.srmkzilla.net/slither_into_open_source/covers/XYIGXmdJVh.png',
-      icons: null,
-      isCompleted: true,
-      isRegClosed: true,
-      maxRsvp: 200,
-      prizes: [
-        {
-          amount: '10,000',
-          description: 'Yeh paise mujhe dedo',
-          asset: 'https://eshan.com',
-          sponsor: 'Sponsor devta hai',
-        },
-      ],
-      slug: 'slither_into_open_source',
-      speakers: [
-        {
-          _id: '6352313443de56b95f8697b8',
-          about:
-            'Shivay Lamba is a software engineer specializing in Web Development, Machine Learning and DevOps. He is a Developer Relations Consultant helping various startups improve their developer experience. He is also an open source contributor, maintainer and mentor.',
-          email: 'shivaylamba@gmail.com',
-          githubLink: 'https://github.com/shivaylamba',
-          image:
-            'https://s3.ap-south-1.amazonaws.com/events.srmkzilla.net/slither_into_open_source/covers/mmb0t413Pw.jpeg',
-          linkedIn: 'https://www.linkedin.com/in/shivaylamba/',
-          name: 'Shivay Lamba',
-          slug: 'slither_into_open_source',
-        },
-      ],
-      startDate: '2022-10-21',
-      tagline:
-        'Now with 100% fewer nightmares of compiler errors! The possibilities to create wonders with open-source are vast. Join us to celebrate Hacktoberfest with a week-long event and give wings to your developer journey!',
-      timeline: [
-        {
-          date: '2022-10-21 18:30',
-          description:
-            'Online interactive workshop and intro. Will you withstand meeting all of SRMKZILLA’s finest?',
-          title: 'Interactive Workshop',
-        },
-        {
-          date: '2022-10-22 00:00',
-          description:
-            'Fun post interactions and easy repository challenges! Contribute, learn and have fun!\n\n21st and 22nd October.',
-          title: 'Easy to medium contributions!',
-        },
-        {
-          date: '2022-10-23 00:00',
-          description:
-            'Crank up the spook! Level up and solve the moderate & hard repository challenges!\n\n23rd to 27th October.',
-          title: 'Hard yet fun contributions!',
-        },
-      ],
-      title: 'SLITHER into OPEN SOURCE',
-    },
+    props: eventDetails[0],
   }
 }
 export default Events
--- shared/components/events/event-prizes.tsx
+++ shared/components/events/event-prizes.tsx
@@ -5,14 +5,18 @@ const PrizePage = ({ event }: { event: EventType }) => {
   const { title, description, slug, prizes } = event
   return (
     <div className="mx-auto mt-16">
-      <h1 className="text-6xl sm:text-6xl font-semibold mt-10 mx-auto text-center md:mb-20">
-        Prizes
-      </h1>
-      <div className="flex flex-col mt-10 justify-center w-full">
-        {prizes.map((i) => {
-          return <PrizesCard prize={i} />
-        })}
-      </div>
+      {prizes[0] && (
+        <>
+          <h1 className="text-6xl sm:text-6xl font-semibold mt-10 mx-auto text-center md:mb-20">
+            Prizes
+          </h1>
+          <div className="flex flex-wrap xl:flex-row flex-col items-center justify-center sm:mt-20 mt-14 pb-10">
+            {prizes.map((i) => {
+              return <PrizesCard prize={i} />
+            })}
+          </div>
+        </>
+      )}
     </div>
   )
 }
