653592b10e53759b9db72598c9fffed48aa8be32
Utkarshini <utkarshininarayan@gmail.com>
2023-08-25 16:20:32 +0530

    chore: update alumni reviews

--- lib/data/reviews.ts
+++ lib/data/reviews.ts
@@ -3,18 +3,18 @@ export const alumniReviews = [
     name: 'Aniruddha Chatterjee',
     src: 'https://srmkzilla.s3.ap-south-1.amazonaws.com/team/2022/aniruddha-chatterjee-president',
     designation: 'President',
-    message: `SRMKZILLA is a place to grow, code & contribute on open source platform. It was a rollercoaster ride for me, where I learnt about both technical and management skills and I can proudly say that it's a team dedication and smart work because of which it is the best technical club in South Indian region. This club played an influential role in my college journey and I'm proud to be a part of the club.`,
+    message: `SRMKZILLA is a place to grow, code & contribute on an open source platform. It was a rollercoaster ride for me, where I learnt about both technical and management skills and I can proudly say that it's a team dedication and smart work because of which it is the best technical club in the South Indian region. This club played an influential role in my college journey and I'm proud to be a part of the club.`,
   },
   {
     name: 'Krishna Priya',
     src: 'https://srmkzilla.s3.ap-south-1.amazonaws.com/team/2022/krishna-priya-vice-president',
     designation: 'Vice President',
-    message: `SRMKZILLA has been an integral part of my college experience. Without it, my journey definitely would not have been the same. It introduced me to so many new avenues, and more importantly, to so many amazing and interesting personalities that still continue to teach and influence me. It was, and will always be, an honor to be a part of the club!`,
+    message: `I don't have to think twice before claiming SRMKZILLA's part in making me who I am. Truly grateful for the people and experiences the club has offered me. Be it managing a well equipped team or making critical decisions, the club has let me find my niche in everything. SRMKZILLA has been and always will be a part of my identity.`,
   },
   {
-    name: 'Anshika Singh',
-    src: 'https://srmkzilla.s3.ap-south-1.amazonaws.com/team/2022/anshika-singh-vice-president',
-    designation: 'Vice President',
+    name: 'Sagnik Biswas',
+    src: 'https://srmkzilla.s3.ap-south-1.amazonaws.com/team/2022/sagnik-biswas-head-of-technical',
+    designation: 'Head of Technical',
     message: `SRMKZILLA is one of the most hardworking and successful clubs of SRM.
     SRMKZILLA is a place where you can explore, learn and implement.
      Leading this club was an amazing feeling and organizing MOZOFEST was one of my favourite days during college time.`,
